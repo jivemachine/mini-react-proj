77e56df47e40b586b4554ba0fba0fc918060ce54
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-27 17:13:41 -0500

    added first pages route for list of users

---
+++ src/pages/Users.jsx
@@ -0,0 +1,14 @@
+const Users = () => {
+    return (
+        <div className="col-md-6">
+            <ul className="list-group">
+                <li className="list-group-item">
+                    Test UserName
+                </li>
+            </ul>
+
+        </div>
+    );
+}
+
+export default Users;

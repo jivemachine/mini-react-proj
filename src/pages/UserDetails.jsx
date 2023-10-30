44bf8fabdcfbd5d4fc6795ab2f0ff8ec2b9e224a
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-30 12:06:01 -0500

    added user state & use effect to fetch user details

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -2,14 +2,28 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from 'react-router-dom';
 
 const UserDetails = () => {
-
     const { userid } = useParams();
+
+    const [details, setDetails] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${userid}`)
+            .then(res => res.json())
+            .then(data => setDetails(data))
+            .catch(event => alert(event.message));
+    }, [userid]);
+
+    // one options for null default value
+    // if(!details) {
+    //     return <h1>Loading...</h1>;
+    // }
 
     return (
         <div className="col-md-6">
             <div className="card shadow rounded">
                 <div className="card-body">
-                    <h4 className="card-title">Username</h4>
+                    <h4 className="card-title">{details?.username}</h4>
+                    <Link to="/users">Go Back</Link>
                 </div>
             </div>
         </div>
